de5d0a092b44ba2dfa27ad211aba4dfcee648e9c
Joaquin <joaq.osu@gmail.com>
2024-04-29 23:14:49 -0600

    Termina seccion 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CounterModule } from './counter/counter.module';
 import { AppRoutingModule } from './app-routing.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppComponent } from './app.component';
+
 
 
 
@@ -20,6 +22,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     CounterModule,
     HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
